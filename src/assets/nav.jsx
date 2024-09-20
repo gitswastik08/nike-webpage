132aca4ad080bc4ca9305e3992fa857fbe2d2e14
Swastik <swastikyadav1237@gmail.com>
2024-09-20 12:45:25 +0530

    react setup

---
+++ src/assets/nav.jsx
@@ -0,0 +1,18 @@
+function nav() {
+    return <div>
+        <nav>
+            <div id="leftnav">
+                <img src="/images/brand_logo.png" alt="" />
+            </div>
+            <ul>
+                <li>menu</li>
+                <li>location</li>
+                <li>about</li>
+                <li>contact</li>
+            </ul>
+            <button>login</button>
+            
+        </nav>
+    </div>
+}
+export default nav;
