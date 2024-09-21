a0d1354c6d99ed771424d9420cbcf4d1790a90bb
Swastik <swastikyadav1237@gmail.com>
2024-09-22 01:52:42 +0530

    complete hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
           YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES,YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.
         </p>
         <div className="shop_cat">
