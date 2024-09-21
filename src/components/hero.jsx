a111d60d99603339ff9891b75db8b12e885bb06b
Swastik <swastikyadav1237@gmail.com>
2024-09-22 01:50:12 +0530

    compeate hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,13 +1,32 @@
-function HeroSection() {
-  return;
-  <div id="hero_c">
-    <h1>YOUR FEET DESERVE THE BEST</h1>
-    <p>
-      YOUR FEET DESERVE THE BEST YOUR FEET DESERVE THE BEST AND WE’RE HERE TO
-      HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP
-      YOU WITH OUR SHOES. Shop Now Category Also Available On
-    </p>
-  </div>;
-}
+import "../assets/app.css";
+function Hero() {
+  return (
+    <main className="hero">
+      <div className="text_section">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="shop_cat">
+          <button id="b1">shop now</button>
+          <button className="b2">category</button>
+        </div>
+        <div className="avbl">
+          <p>Also available on</p>
+          <div className="avbl_logo">
+            <img src="/images/flipkart.png" alt="" />
+            <img src="/images/amazon.png" alt="" />
 
-export default HeroSection;
+          </div>
+          
+        </div>
+      </div>
+      <div id="shoe_section">
+        <img src="/images/shoe_image.png" alt="" />
+      </div>
+    </main>
+  );
+}
+export default Hero;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/index.css";
 import Nav from "./components/nav";
-import HeroSection from "./components/hero";
+import Hero from "./components/hero";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div id="main">
     <Nav />
-    <HeroSection />
+    <Hero />
     </div>
   </StrictMode>
 );
